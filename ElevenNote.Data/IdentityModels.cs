94036c832e2567c158f4d98ba9cc0acf38c16a3e
Ben Peterson <ben.micah.peterson@gmail.com>
2017-04-12 11:53:07 -0400

    Added Database Configuration

--- ElevenNote.Data/IdentityModels.cs
+++ ElevenNote.Data/IdentityModels.cs
@@ -3,6 +3,8 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity.ModelConfiguration;
+using System.Data.Entity.ModelConfiguration.Conventions;
 
 namespace ElevenNote.Data
 {
@@ -29,5 +31,32 @@ namespace ElevenNote.Data
         {
             return new ApplicationDbContext();
         }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Configurations.Add(new IdentityUserLoginConfiguration()).Add(new IdenetityUserRoleConfiguration());
+        }
     }
+  
+   
+    public class IdentityUserLoginConfiguration 
+        : EntityTypeConfiguration<IdentityUserLogin>
+    {
+        public IdentityUserLoginConfiguration()
+        {
+            HasKey(iul => iul.UserId);
+        }
+    }
+
+    public class IdenetityUserRoleConfiguration 
+        : EntityTypeConfiguration<IdentityUserRole>
+    {
+        public IdenetityUserRoleConfiguration()
+        {
+            HasKey(iur => iur.RoleId);
+        }
+    }
+
+    
+
 }

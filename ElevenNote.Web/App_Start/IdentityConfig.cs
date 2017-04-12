c813e6d2977a81a978f7d29ae7849c7f45358ec6
Ben Peterson <ben.micah.peterson@gmail.com>
2017-04-12 10:29:18 -0400

    Initial Commit

--- ElevenNote.Web/App_Start/IdentityConfig.cs
+++ ElevenNote.Web/App_Start/IdentityConfig.cs
@@ -55,9 +55,9 @@ namespace ElevenNote.Web
             {
                 RequiredLength = 6,
                 RequireNonLetterOrDigit = true,
-                RequireDigit = true,
-                RequireLowercase = true,
-                RequireUppercase = true,
+                RequireDigit = false,
+                RequireLowercase = false,
+                RequireUppercase = false,
             };
 
             // Configure user lockout defaults

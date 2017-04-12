e6a73d494ecabe6ec5e426e2cb4d808ede9ed3c7
Ben Peterson <ben.micah.peterson@gmail.com>
2017-04-12 15:07:39 -0400

    Created Note List Model and List Page

---
+++ ElevenNote.Web/Controllers/NoteController.cs
@@ -0,0 +1,21 @@
+﻿using ElevenNote.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElevenNote.Web.Controllers
+{
+    [Authorize]
+    public class NoteController : Controller
+    {
+        public ActionResult Index()
+        {
+            //TODO ASK DAVE HOW THIS IS CONNECTING TO NOTE VIEWS
+            var model = new NoteListItem[0];
+            return View(model); 
+            
+        }
+    }
+}

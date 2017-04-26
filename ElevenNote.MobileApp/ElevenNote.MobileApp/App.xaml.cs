755944c27c2030084f0cb895fcaaae6c8d317c45
ben.micah.peterson <ben.micah.peterson@gmail.com>
2017-04-26 17:08:32 -0400

    Added Login Functionality

--- ElevenNote.MobileApp/ElevenNote.MobileApp/App.xaml.cs
+++ ElevenNote.MobileApp/ElevenNote.MobileApp/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ElevenNote.MobileApp.ExternalServices;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,11 +10,14 @@ namespace ElevenNote.MobileApp
 {
 	public partial class App : Application
 	{
+
+        internal static readonly NoteService NoteService = new NoteService();
+
 		public App ()
 		{
 			InitializeComponent();
 
-			MainPage = new ElevenNote.MobileApp.MainPage();
+            MainPage = new NavigationPage(new LoginPage());
 		}
 
 		protected override void OnStart ()
---
+++ ElevenNote.MobileApp/ElevenNote.MobileApp/Models/OauthBearerTokenResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElevenNote.MobileApp.Models
+{
+    internal class OauthBearerTokenResponse
+    {
+        //Properties are named liked this 
+        //because when the JSON comes back they
+        //Will be in that format. And if named the same
+        //We can use the values as is, not requiring translation
+        public string access_token { get; set; }
+        public string token_type { get; set; }
+        public int expires_in { get; set; }
+        public string userName { get; set; }
+    }
+}
